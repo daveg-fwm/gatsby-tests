7008b37f2fcdaf0cf317c13e7983ce6850bd0279
daveg-fwm <wddavegreen@gmail.com>
2020-11-28 16:44:33 +0200

    chore: add project dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Tests`,
+    description: `A repo to try out unused or experimental features, functionality and project strategy.`,
+    author: `@daveg-fwm`,
   },
   plugins: [
+    {
+			resolve: 'gatsby-source-contentful',
+			options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        environment: process.env.CONTENTFUL_ENVIRONMENT,
+        host: process.env.NODE_ENV === 'development' ? 'preview.contentful.com' : 'cdn.contentful.com',
+      },
+		},
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +38,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
